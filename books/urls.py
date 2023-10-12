a2190bd6eb52071ebb8cee6ff97b778d971866d1
Hadi-Abedah2 <hadi.abedah99@gmail.com>
2023-10-12 05:55:50 +0300

    I added link to make review in the book_detail.html"

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, ReviewCreateView
 
 
 
@@ -7,5 +7,7 @@ from .views import BookListView, BookDetailView
 urlpatterns = [
 
     path("", BookListView.as_view(), name="book_list"),
-    path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail")
+    path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("<uuid:pk>/review/new", ReviewCreateView.as_view(), name="review_create"),
+
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,11 @@
 from typing import Any
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render
 from django.views import generic
-from .models import Book
+from .models import Book, Review
+from .forms import ReviewForm
 # Create your views here.
 
 
@@ -16,5 +20,23 @@ class BookDetailView(generic.DetailView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         book = Book.objects.get(pk=self.kwargs.get('pk'))
         reviews = book.reviews.all()
-        context = {'book':book, 'reviews':reviews}
-        return context
+        review_form = ReviewForm()
+        context = {'book':book, 'reviews':reviews, 'review_form':review_form}
+        return context 
+    
+
+
+class ReviewCreateView(generic.CreateView):
+    model = Review
+    form_class  = ReviewForm 
+    #success_url = reverse_lazy('book_list')
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.book = Book.objects.get(pk=self.kwargs.get('pk'))
+        self.object = form.save()
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('book_detail', kwargs={'pk': self.kwargs.get('pk')})
+    
+
+    
